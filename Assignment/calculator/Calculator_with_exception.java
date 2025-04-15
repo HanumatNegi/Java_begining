088bc95dd2beff4838f11e81b66f2b282f553c37
Hanumat Singh Negi <149346310+HanumatNegi@users.noreply.github.com>
2025-04-15 08:57:11 +0530

    Update Calculator_with_exception.java

--- Assignment/calculator/Calculator_with_exception.java
+++ Assignment/calculator/Calculator_with_exception.java
@@ -28,23 +28,20 @@ class InvalidInputException extends Exception {
 public class Calculator_with_exception {
 
     // Validation method
-    public static void exceptionHandling(int a, int b, String operation) throws InvalidInputException, MaxInputException, MaxMultiplierReachedException, CannotDivideByZeroException {
-        if ((a == 8 && b == 9) || (a == 9 && b == 8)) {
+    public static void exceptionHandling(int a, int b, String operation) throws Exception {
+        if ((a == 8 && b == 9) || (a == 9 && b == 8))
             throw new InvalidInputException("Invalid combination of inputs: 8 and 9 together.");
-        }
 
-        if (a > 100000 || b > 100000) {
+        if (a > 100000 || b > 100000)
             throw new MaxInputException("Input exceeds the maximum allowed value of 100000.");
-        }
 
-        if (operation.equals("*") && (a > 7000 || b > 7000)) {
+        if ("*".equals(operation) && (a > 7000 || b > 7000))
             throw new MaxMultiplierReachedException("Multiplication inputs must be <= 7000.");
-        }
 
-        if (operation.equals("/") && b == 0) {
+        if ("/".equals(operation) && b == 0)
             throw new CannotDivideByZeroException("Cannot divide by zero.");
-        }
     }
+
 
     // Operation methods using internal try-catch
     public static void add(int a, int b) {
